dbbea2c82090e752e0e256ebe0bb4a82bd10e600
Vladyslav Korol <95221193+ChocolateMagnate@users.noreply.github.com>
2022-10-18 12:53:45 +0000

    saved changes.

--- Tests/Compiler/Lexer.cc
+++ Tests/Compiler/Lexer.cc
@@ -2,12 +2,13 @@
 #include <string>
 #include <list>
 #include <iostream>
-#define print(x) for (auto item : x) std::cout << item.first << item.second << "\n"
+#define print(x) for (auto item : x) std::cout << item.first << "\n"
 int main(){
-    std::string code = "print(\"Hello World!\") #Hello world program";
-    std::list<std::string> components = {"print", "(", "\"Hello ",  "World!\"", ")", " ", "#Hello world program"};
-    for (auto x : mamba::extractStrings(&components))
-        std::cout << x.first << " " << x.second << "\n";
-    std::cout << "-------------------\n";
+    std::string code = "print(\"Hello World!\" + \"We are from Ukraine!\") #Hello world program";
+    std::string commentless = mamba::clearComments(code).first;
+    std::cout << commentless << "\n";
+    auto components = mamba::extractStrings(commentless);
+    print(components);
+    std::cout << "-----------------" << std::endl;
     return 0;
 }
