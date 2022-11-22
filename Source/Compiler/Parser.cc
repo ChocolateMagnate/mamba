30025fdc7220099e1fcd78a9a81a8cc46f4529cc
Vladyslav Korol <pl5_kvya@student.ztu.edu.ua>
2022-11-22 17:29:24 +0200

    added AST class and declared a few methods.

--- Source/Compiler/Parser.cc
+++ Source/Compiler/Parser.cc
@@ -12,6 +12,32 @@ namespace mamba {
      * permitted by the language. At the end, an Abstract Syntax Tree is 
      * generated that furthermore is converted into the bytecode. All sorts
      * of errors and warnings are also raised on this stage. */
+    
+    enum class TokenRelation {
+        Addition, Substraction, Multiplication, TrueDivision, FloorDivision, 
+        Assign, AddTo, SubtractFrom, MultiplyBy, DivideBy, Access,/*the dot operator*/
+        Import, 
+    }
+    /// @brief The base AST class used to represent the flow of the program.
+    class AbstractSyntaxTree {
+        unsingned int size; //Indicates the total size of lexemes.
+        Lexeme *start, *counter;
+    public:
+        AbstractSyntaxTree(unsigned int capacity = 400){
+            this->size = 0;
+            this->start = (Lexeme*)malloc(capacity);
+            this->counter = this->start;
+        }
+        ~AbstractSyntaxTree() {
+            this->size = 0;
+            free(this->start);
+        }
+        /// @brief Appends a lexeme as a child of its parent.
+        Lexeme* child(const Lexeme* parent, const Lexeme& lexeme, const TokenRelation relation) {
+            
+            return (Lexeme*)this->counter;
+        }
+    }
 
     /// @brief Prints the compile-time warning into the terminal.
     /// @param message The warning message to be printed.
@@ -26,7 +52,7 @@ namespace mamba {
     /// @param message The error message to be printed.
     /// @param line The line number where the error occured.
     /// @param offset The column in the line above.
-    void raiseError(const char* message, const char* line, int offset) {
+    void raiseError(const char* message, const char* line, const int offset) {
         std::cout << line << "|" << line << "\n"
                 << std::string(offset, ' ') << "^\n" << message << std::endl;
         exit(1);
